988c88fd05efcb1207be56446a63d567a13395c7
DanDeLuna <danieldeluna4@gmail.com>
2023-05-11 14:59:15 -0600

    Started to make inquirer questions for db

--- server.js
+++ server.js
@@ -9,3 +9,56 @@ const connection = mysql.createConnection({
   password: 'password',
   database: 'employeellc_db',
 });
+connection.connect(err => {
+  if (err) throw err;
+  console.log("Employee DB ");
+  questions();
+});
+
+const questions = () => {
+  inquirer.prompt({
+      message: 'Options for DB?',
+      name: 'menu',
+      type: 'list',
+      choices: [ 
+        'View all departments',
+        'View all jobs',
+        'View all employees',
+        'Add a department',
+        'Add a job',
+        'Add an employee',
+        'Update an employee job',
+        'Exit',
+      ],
+    })
+    .then(response => {
+        switch (response.menu) {
+        case 'View all departments':
+          viewDep();
+          break;
+        case 'View all jobs':
+          viewJob();
+          break;
+        case 'View all employees':
+          viewEmp();
+          break;
+        case 'Add a department':
+          addDep();
+          break;
+        case 'Add a job':
+          addJob();
+          break;
+        case 'Add an employee':
+          addEmp();
+          break;
+        case 'Update employee job':
+          updateJob();
+          break;
+        case "Exit":
+          connection.end();
+          break;
+        default:
+          connection.end();
+      }
+    });
+};
