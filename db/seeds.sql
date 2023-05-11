7598126eebc9ec072e354195caa3381cb6ddc523
DanDeLuna <danieldeluna4@gmail.com>
2023-05-11 13:49:42 -0600

    Added requires for app SQL,connection, and contable

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO department (name)
-VALUES  ('Finace'), 
+VALUES  ('Finance'), 
         ('Accounting'), 
         ('Engineering'), 
         ('Human Resources'),
@@ -7,14 +7,14 @@ VALUES  ('Finace'),
 
 
 INSERT INTO role (title, salary, department_id) 
-VALUES  ('Finace Manager', 150000, 1),
-        ('Finace Analyst', 55000, 1),
+VALUES  ('Finance Manager', 150000, 1),
+        ('Finance Analyst', 55000, 1),
         ('Accounting Manager', 110000, 2),
         ('Accountant', 60000, 2),
         ('QA Lead', 150000, 3),
         ('QA Engineer', 80000, 3),
         ('HR Manager', 85000, 4),
-        ('Recruiter', 50000, 4),
+        ('Talent specialist', 50000, 4),
         ('Corporate Lawyer', 130000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
@@ -26,4 +26,4 @@ VALUES  ('Peter', 'Parker', 1, NULL),
         ('Alexander', 'TheGreat', 6, 5),
         ('Larry', 'Bird', 7, NULL),
         ('Mary', 'Jane', 8, 7),
-        ('Joe', 'Dirt', 9, NULL);
+        ('Joe', 'Dirt', 9, NULL); 
